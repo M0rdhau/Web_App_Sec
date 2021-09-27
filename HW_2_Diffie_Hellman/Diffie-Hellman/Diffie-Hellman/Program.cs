4f336ba65387e561c851deaf1971422f195e7d25
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-09-27 10:14:15 +0300

    Started working on HW2 - Diffie Hellman

---
+++ HW_2_Diffie_Hellman/Diffie-Hellman/Diffie-Hellman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diffie_Hellman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
