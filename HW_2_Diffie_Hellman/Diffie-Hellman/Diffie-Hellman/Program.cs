0562e9caef3e27a60f70e0179813af3def6d37ef
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-10-08 17:25:31 +0300

    Init webapp. Start working on DH and RSA

--- HW_2_Diffie_Hellman/Diffie-Hellman/Diffie-Hellman/Program.cs
+++ HW_2_Diffie_Hellman/Diffie-Hellman/Diffie-Hellman/Program.cs
@@ -6,7 +6,16 @@ namespace Diffie_Hellman
     {
         static void Main(string[] args)
         {
+            //sqrt of maxvalue is more than actual sqrt
+            //so square(sqrt(maxvalue)) > maxvalue == overflow
             Console.WriteLine("Hello World!");
+            ulong ceiling =(ulong) Math.Floor(Math.Sqrt(ulong.MaxValue));
+            ulong notCeiling = (ceiling * ceiling) - 1;
+            ulong otherCeiling = (ulong) Math.Floor(Math.Sqrt(ceiling));
+            Console.WriteLine(Math.Sqrt(ulong.MaxValue));
+            Console.WriteLine(ceiling);
+            Console.WriteLine(notCeiling);
+            Console.WriteLine(otherCeiling);
         }
     }
 }
