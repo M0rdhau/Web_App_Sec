77b678717c008e382923a34ae2fe67a660da4008
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-10-19 13:29:30 +0300

    completed main menu

--- src/commandline.go
+++ src/commandline.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
-	"github.com/m0rdhau/Web_App_Sec/src/utils"
+	"github.com/m0rdhau/Web_App_Sec/src/menus"
 )
 
-func rotateRune(char rune, rotation uint32) {
-	char = char + rune(rotation)
-	fmt.Println(char)
-}
-
 func main() {
-	something := -3
-	otherthing := something % 4
-	somestring := "asdasdasd"
-	otherstring := "asd"
-	fmt.Println(utils.DoVigenere(somestring, otherstring, utils.PlainText))
-	fmt.Println(somestring[2:])
-	fmt.Println(otherthing)
-	fmt.Println('a')
-	rotateRune('a', 1)
+	buff := ""
+	reader := bufio.NewReader(os.Stdin)
+	for buff != "X" {
+		fmt.Println("===============================")
+		fmt.Println("Please Select the encryption:")
+		fmt.Println("===============================")
+		fmt.Println("[C]aesar")
+		fmt.Println("[V]igenere")
+		fmt.Println("Or would you like to e[X]it?")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Something went wrong")
+			continue
+		}
+		buff = strings.ToUpper(strings.TrimSpace(input))
+		if buff == "X" {
+			continue
+		}
+		if buff != "C" && buff != "V" {
+			continue
+		}
+		var enctype menus.EncType
+		if buff == "C" {
+			enctype = menus.Caesar
+		} else {
+			enctype = menus.Vigenere
+		}
+		control, _ := menus.EncryptDecryptMenu(enctype)
+		if control {
+			buff = ""
+		} else {
+			buff = "X"
+		}
+	}
 }
