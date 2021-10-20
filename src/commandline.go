9b2c2dc3dc4215755be0a163d0b235154e5160e6
Dachi Mshvidobadze <dachimshvidobadze@gmail.com>
2021-10-20 22:49:15 +0300

    With this kind of modpow, it's infinitely quicker to check for primes the 'dumber' way

--- src/commandline.go
+++ src/commandline.go
@@ -3,13 +3,30 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 
+	"github.com/m0rdhau/Web_App_Sec/src/cryptoutils"
 	"github.com/m0rdhau/Web_App_Sec/src/menus"
 )
 
 func main() {
+	//set the seed once
+	rand.Seed(time.Now().UnixNano())
+	maxuint := uint64(math.Sqrt(float64(math.MaxUint64)))
+	p := rand.Uint64()
+	if p%2 == 0 {
+		p--
+	}
+	p %= maxuint
+	fmt.Println(cryptoutils.TestPrimeSlow(p))
+	cryptoutils.TestPrime(p, 1)
+
+	fmt.Println(cryptoutils.GeneratePrime())
+	return
 	buff := ""
 	reader := bufio.NewReader(os.Stdin)
 	for buff != "X" {
--- src/cryptoutils/cryptoutils.go
+++ src/cryptoutils/cryptoutils.go
@@ -1,10 +1,12 @@
 package cryptoutils
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
-	"time"
 )
+
+const GOROUTINE_NUM int = 1000
 
 func ExtendedEuclid(e uint64, d uint64) uint64 {
 	x := [3]uint64{1, 0, d}
@@ -40,8 +42,8 @@ func ExtendedEuclid(e uint64, d uint64) uint64 {
 
 // n is a possible prime
 // n-1 = 2^r * d + 1
-func FactorPossiblePrime(n uint64) (uint64, int) {
-	r := 0
+func FactorPossiblePrime(n uint64) (uint64, uint64) {
+	r := uint64(0)
 	d := n - 1
 	for d%2 != 1 {
 		r++
@@ -50,29 +52,74 @@ func FactorPossiblePrime(n uint64) (uint64, int) {
 	return d, r
 }
 
+func GeneratePrime() uint64 {
+	maxuint := uint64(math.Sqrt(float64(math.MaxUint64)))
+	p := rand.Uint64()
+	if p%2 == 0 {
+		p--
+	}
+	p %= maxuint
+	fmt.Println("prime?")
+	for !TestPrime(p, 10) {
+		fmt.Println("not prime", p)
+		p = rand.Uint64()
+		if p%2 == 0 {
+			p--
+		}
+		p %= maxuint
+	}
+	return p
+}
+
+func SingleTest(n uint64, d uint64, r uint64, channel chan bool) {
+	fmt.Println("loop")
+	//pick a random integer a in the range [2, n − 2]
+	// Just to make sure something weird doesn't happen
+	acounter := 0
+	a := uint64(0)
+	for a = rand.Uint64() % n; a <= 1 || a >= n-1; {
+		fmt.Println("looking for a")
+		a = rand.Uint64()
+		a = a % n
+		acounter++
+	}
+	// x := (a ^ d)%n
+	x := Modpow(n, d, a)
+	fmt.Println("modpow 1 done", x)
+	if x == 1 || x == n-1 {
+		channel <- true
+		return
+	}
+	shouldContinue := false
+	for j := 0; uint64(j) < r-1; j++ {
+		//x = (x^2)%n
+		x = (x * x) % n
+		if x == n-1 {
+			shouldContinue = true
+		}
+	}
+	if shouldContinue {
+		channel <- true
+		return
+	}
+	channel <- false
+}
+
 // uses miller-rabin theorem
 // n is a prime to test,k is the times we repeat the test
 // a is a random number - possibly a witness
 func TestPrime(n uint64, k int) bool {
+	// write n as (2^r)·d + 1 with d odd (by factoring out powers of 2 from n − 1)
 	d, r := FactorPossiblePrime(n)
-	seconds := time.Now().Unix()
+	fmt.Println("factored", d, r)
+	channel := make(chan bool)
 	for i := 0; i < k; i++ {
-		rand.Seed(seconds)
-		a := rand.Uint64()
-		for a > 1 && a < n-1 {
-			a = rand.Uint64()
+		go SingleTest(n, d, r, channel)
+	}
+	for i := 0; i < k; i++ {
+		if !<-channel {
+			return false
 		}
-		x := a ^ d%n
-		if x == 1 || x == n-1 {
-			continue
-		}
-		for j := 0; j < r-1; j++ {
-			x = x ^ 2%n
-			if x == n-1 {
-				continue
-			}
-		}
-		return false
 	}
 	return true
 }
@@ -99,7 +146,7 @@ func Modpow(mod uint64, pow uint64, number uint64) uint64 {
 		return number
 	}
 
-	var carryover uint64 = 0
+	carryover := number
 	for {
 		if pow == 1 {
 			return carryover % mod
