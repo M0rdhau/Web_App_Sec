aac335b55d54a2f6eefa92503383fc8831a9eba9
Dachi Mshvidobadze <dachi.mshvidobadze@pipedrive.com>
2022-05-01 20:25:00 +0300

    mysql user and pass now are env vars

--- Web_App_Sec.go
+++ Web_App_Sec.go
@@ -10,16 +10,22 @@ import (
 	"github.com/m0rdhau/Web_App_Sec/src/routes"
 )
 
+const MYSQL_USER = "MYSQL_USER"
+const MYSQL_PASSWORD = "MYSQL_PASSWORD"
+
 func main() {
 	args := os.Args
 	//set the seed once
 	rand.Seed(time.Now().UnixNano())
 
+	mysql_user := os.Getenv(MYSQL_USER)
+	mysql_password := os.Getenv(MYSQL_PASSWORD)
+
 	// Check if we're running console app or not
 	if len(args) > 1 && args[1] == "console" {
 		menus.MainMenu()
 	} else {
-		db.InitDatabase()
+		db.InitDatabase(mysql_user, mysql_password)
 		routes.Route()
 	}
 }
--- src/db/db.go
+++ src/db/db.go
@@ -10,10 +10,10 @@ import (
 
 var GlobalDB *gorm.DB
 
-func InitDatabase() (err error) {
+func InitDatabase(user string, password string) (err error) {
 	maxErrors := 10
 	currErrors := 0
-	dsn := "root:toor@tcp(127.0.0.1:3306)/appsec?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := user + ":" + password + "@tcp(127.0.0.1:3306)/appsec?charset=utf8mb4&parseTime=True&loc=Local"
 	for currErrors < maxErrors {
 		GlobalDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 		if err != nil {
